a5e99a5b7f24add04b81d4373c6e0a1ac921f490
Wiedjaja Atmadja <swiedjaja@binus.ac.id>
2023-02-27 23:56:31 +0700

    add theme data, appBar

--- lib/main.dart
+++ lib/main.dart
@@ -9,12 +9,28 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.green,
       ),
+      home: Scaffold(
+        appBar: AppBar(title: const Text('My First App From Scratch')),
+        body: const Center(child: Text('Hello World!')),
+        // body: Container(
+        //   color: Colors.green,
+        //   child: const Center(
+        //     child: Text(
+        //       'Hello World!',
+        //       style: TextStyle(
+        //         color: Colors.white,
+        //         fontSize: 30,
+        //         fontWeight: FontWeight.bold,
+        //       ),
+        //     ),
+        //   ),
+        // )
+      ),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
